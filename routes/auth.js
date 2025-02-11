5b7bab28935fec91acc7ea487cd74cf82480bf3a
Jacinthsharon <jacinthsharon53@gmail.com>
2025-02-11 10:19:28 +0530

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,6 @@
 const express = require("express");
-//const bcrypt = require("bcryptjs");
-const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-const User = require("../models/User"); 
+const User = require("../models/User");
 
 const router = express.Router();
 
